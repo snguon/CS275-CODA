2a096414927a233c04eff9090de323b288f26e07
Duncan Enzmann <djenzmann@gmail.com>
2018-10-30 11:55:24 -0400

    change splash len

--- app/src/main/java/com/devteam/coda/coda/MainActivity.java
+++ app/src/main/java/com/devteam/coda/coda/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
