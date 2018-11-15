c9583b6bdc23d880a27c7497c27a12bcc5c5e46f
Sam Nguon <snguon@uvm.edu>
2018-11-15 10:35:05 -0500

    Made it so RegisterActivity links to the register website

--- app/src/main/java/com/devteam/coda/coda/RegisterActivity.java
+++ app/src/main/java/com/devteam/coda/coda/RegisterActivity.java
@@ -2,12 +2,24 @@ package com.devteam.coda.coda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        webview =(WebView)findViewById(R.id.webView);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("http://nrlawren.w3.uvm.edu/cs275_CODA/index.php");
     }
 }
