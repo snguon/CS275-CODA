0c78904a9a58b4d0d0541ca8bd9e3bf559c0243a
Duncan Enzmann <djenzmann@gmail.com>
2018-11-27 11:09:31 -0500

    toolbar
    added toolbar to each page

--- app/src/main/java/com/devteam/coda/coda/WatchActivity.java
+++ app/src/main/java/com/devteam/coda/coda/WatchActivity.java
@@ -2,12 +2,42 @@ package com.devteam.coda.coda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class WatchActivity extends AppCompatActivity {
+    private MenuItem myActionMenuItem;
+    private EditText myActionEditText;
+
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int res_id = item.getItemId();
+        if(res_id == R.id.action_settings){
+            Toast.makeText(getApplicationContext(),
+                    "You selected settings option", Toast.LENGTH_LONG).show();
+        }
+        return true;
     }
 }
