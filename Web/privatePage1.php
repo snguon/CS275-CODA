515453e490a14afbba2640f3f2ed19d0dc397faf
Nicholas Lawrence <nrlawren@uvm.edu>
2018-12-07 17:38:19 -0500

    Update privatePage1.php
    PDF open on the website

--- Web/privatePage1.php
+++ Web/privatePage1.php
@@ -2,16 +2,22 @@
 session_start();
 include "top.php";
 ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <body>
-	<h1>Private Page 1</h1>
+	<h1>Discharge Instructions</h1>
+
 <?php
-echo "Active User " . $_SESSION["activeUser"]
+echo "Welcome Back, " . $_SESSION["activeUser"]
 ?>
 <?php if ($_SESSION["login"] !=  1){
 	print ("Must Login First2");
 	include ("login.php");
 }?>
 
+<object data="http://snguon.w3.uvm.edu/cs275/discharge_instructions.pdf" type="application/pdf" width="100%" height="1000">
+<a href="http://snguon.w3.uvm.edu/cs275/discharge_instructions.pdf">test.pdf</a>
 
+<div class="view" style="background-image: linear-gradient(to right, #789cca, #5374a7, #3e67a1); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+</object>
 </body>
 </html>
